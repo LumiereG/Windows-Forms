eb7b83429eed12ecd56fdbc41b6b7ac2309f5112
Svetlana <svetagridina122@gmail.com>
2025-04-11 15:46:05 +0200

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -1,94 +1,35 @@
-using System.Diagnostics;
-using System.Globalization;
-
-namespace lab4PwSG
+﻿namespace lab2
 {
     public partial class Form1 : Form
     {
-        private string file;
+        private List<Vial> vials; // To easily access the vials
+        private Random random = new Random();
+        private List<Color> potionColors = new List<Color> {
+            Color.Red, Color.Blue, Color.Green, Color.Yellow, Color.Purple, Color.Orange
+        };
         public Form1()
         {
             InitializeComponent();
-            this.FormClosing += OnFormClosing;
+            InitializeVials();
         }
 
-        private void OnFormClosing(object sender, FormClosingEventArgs e)
+        private void InitializeVials()
         {
-            var result = MessageBox.Show("Unsaved changes will be lost.\nAre you sure you want to close the app?",
-                                         "Exit", MessageBoxButtons.YesNo);
-            if (result == DialogResult.No)
-                e.Cancel = true;
-        }
+            // Ustawienie kontrolki vial1, vial2 itd. w liście
+            vials = new List<Vial> { vial1, vial2, vial3, vial4 }; // Załóżmy, że masz vial1, vial2 itd.
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            var addTaskDialog = new Form2(monthCalendar1.SelectionStart);
-            if (addTaskDialog.ShowDialog() == DialogResult.OK)
+            // Dla każdej fiolki w liście vials
+            foreach (var vial in vials)
             {
-                string date = addTaskDialog.SelectedDate.ToShortDateString();
-                string note = addTaskDialog.TaskDescription;
-                ListViewItem item = new ListViewItem(new[] { note, date });
-                listView1.Items.Add(item);
-            }
+                vial.Size = new Size(100, 200); // Ustawiamy rozmiar fiolki
+                vial.Location = new Point(120 * vials.IndexOf(vial), 50); // Pozycja fiolki na formularzu
 
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            foreach (ListViewItem item in listView1.SelectedItems)
-                listView1.Items.Remove(item);
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            using (OpenFileDialog openFileDialog = new OpenFileDialog())
-            {
-                openFileDialog.Filter = "CSV Files (*.csv)|*.csv";
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                // Dodajemy losowe segmenty
+                for (int j = 0; j < 3; j++) // Zakładając, że chcemy 3 losowe segmenty w fiolce
                 {
-                    string lastLoadedFile = openFileDialog.FileName;
-                    listView1.Items.Clear();
-                    foreach (var line in File.ReadLines(lastLoadedFile))
-                    {
-                        var parts = line.Split(',');
-                        if (parts.Length == 3)
-                        {
-                            var item = new ListViewItem("") { Checked = bool.Parse(parts[0]) };
-                            item.SubItems.Add(parts[1]);
-                            item.SubItems.Add(parts[2]);
-                            listView1.Items.Add(item);
-                        }
-                    }
+                    vial.AddSegment(potionColors[random.Next(potionColors.Count)]);
                 }
             }
-        }
-
-        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
-            {
-                saveFileDialog.Filter = "CSV Files (*.csv)|*.csv";
-                saveFileDialog.Title = "Save Task List";
-
-                if (saveFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    file = saveFileDialog.FileName;
-                    if (string.IsNullOrEmpty(file)) return;
-                    using (StreamWriter writer = new StreamWriter(file))
-                    {
-                        foreach (ListViewItem item in listView1.Items)
-                        {
-                            writer.WriteLine($"{item.Checked},{item.SubItems[0].Text},{item.SubItems[1].Text}");
-                        }
-                    }
-                }
-            }
-            MessageBox.Show("File saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
---
+++ Vial.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class Vial : UserControl
+    {
+        public int MaxSegments { get; set; } = 4; // Default value for the number of segments
+        public List<Color> Segments { get; set; } = new List<Color>();
+        public Vial()
+        {
+            InitializeComponent();
+        }
+
+        public void AddSegment(Color color)
+        {
+            if (Segments.Count < MaxSegments)
+            {
+                Segments.Add(color);
+                this.Invalidate(); // Pererysowywanie fioletki po dodaniu segmentu
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+
+            int vialWidth = this.Width;
+            int vialHeight = this.Height;
+            float segmentHeight = (float)vialHeight / MaxSegments;
+
+            using (Pen outlinePen = new Pen(Color.Black, 2))
+            {
+                g.DrawRectangle(outlinePen, 0, 0, vialWidth, vialHeight);
+            }
+
+            // Rysowanie segmentów
+            for (int i = 0; i < Segments.Count; i++)
+            {
+                SolidBrush brush;
+
+                if (i == 0) // Jeśli to pierwszy segment, rysujemy pustą (przezroczystą) komórkę
+                {
+                    brush = new SolidBrush(Color.Transparent); // Używamy przezroczystego koloru
+                }
+                else
+                {
+                    // Inne segmenty rysujemy w odpowiednim kolorze
+                    brush = new SolidBrush(i - 1 < Segments.Count ? Segments[i - 1] : Color.Transparent);
+                }
+
+                // Rysowanie segmentu
+                g.FillRectangle(brush, 0, i * segmentHeight, vialWidth, segmentHeight);
+            }
+        }
+    }
+}
